f6e4761de8f531876c9a84635d22366af2ac43c5
aysmd <aysmd@DESKTOP-V34BDJL>
2021-07-11 17:46:35 +0700

    Add project files.

---
+++ ApaYah/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApaYah.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Roles> Roles { get; set; }
+    }
+}
